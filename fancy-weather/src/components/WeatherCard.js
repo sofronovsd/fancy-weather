ea6dbf14b92c34ea2dcaea6d7abd26d99d86989e
Semyon Sofronov <sofronov.semyon@gmail.com>
2020-05-27 19:58:41 +0400

    refactor: move temps into separate place

--- fancy-weather/src/components/WeatherCard.js
+++ fancy-weather/src/components/WeatherCard.js
@@ -4,6 +4,10 @@ import Moment from "react-moment";
 import 'moment-timezone';
 
 export default function WeatherCard( { data, isC } ) {
+    const temp = data.temp && (isC ? Math.floor(data.temp.value)
+        : Math.floor((data.temp.value * 9) / 5 + 32));
+    const feelsLike = data.feels_like && (isC ? Math.floor(data.feels_like.value)
+        : Math.floor((data.feels_like.value * 9) / 5 + 32));
     return (
         <Card>
             <Card.Body>
@@ -17,12 +21,12 @@ export default function WeatherCard( { data, isC } ) {
                 </p>
                 <div className="flex-container">
                     <p className="card__degree">
-                        {data.temp && (isC ? Math.floor(data.temp.value) : Math.floor((data.temp.value * 9) / 5 + 32))}
+                        {temp}
                         <sup>&deg;{isC ? `C` : `F`}</sup>
                     </p>
                     <div className="card__summary text text__medium">
                         <p>{data.weather_code && data.weather_code.value.replace('_', ' ')}</p>
-                        <p>Feels like: {data.feels_like && (isC ? Math.floor(data.feels_like.value) : Math.floor((data.feels_like.value * 9) / 5 + 32))}<sup>&deg;{isC ? `C` : `F`}</sup></p>
+                        <p>Feels like: {feelsLike}<sup>&deg;{isC ? `C` : `F`}</sup></p>
                         <p>Wind: {data.wind_speed && Math.floor(data.wind_speed.value)} m/s</p>
                         <p>Humidity: {data.humidity && Math.floor(data.humidity.value)}%</p>
                     </div>
